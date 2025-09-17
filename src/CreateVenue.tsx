f134a79ad231b4e86796aa02418b19b96de86021
John Evans <38507954+King-Mob@users.noreply.github.com>
2025-09-17 23:12:09 +0100

    change what happens when submissions are closed

--- src/CreateVenue.tsx
+++ src/CreateVenue.tsx
@@ -3,153 +3,250 @@ import { useNavigate } from "react-router";
 import { postVenue, postImage } from "./requests";
 import { type user, MIDNIGHT13THUTC, ALL_DAY_SLOTS } from "./types";
 
-export function SlotButton({ slotOn, slot, toggleSlot }: { slotOn: boolean, slot: Date, toggleSlot: (slot: Date) => void }) {
-    return <div className="slot-container">
-        <button
-            className={`slot-toggle ${slotOn ? "slot-selected" : "slot-unselected"}`}
-            onClick={() => toggleSlot(slot)}
-        >
-            {Object.values(ALL_DAY_SLOTS).includes(slot.getTime()) ? "ALL DAY" : slot.toTimeString().slice(0, 5)}
-        </button>
+export function SlotButton({
+  slotOn,
+  slot,
+  toggleSlot,
+}: {
+  slotOn: boolean;
+  slot: Date;
+  toggleSlot: (slot: Date) => void;
+}) {
+  return (
+    <div className="slot-container">
+      <button
+        className={`slot-toggle ${
+          slotOn ? "slot-selected" : "slot-unselected"
+        }`}
+        onClick={() => toggleSlot(slot)}
+      >
+        {Object.values(ALL_DAY_SLOTS).includes(slot.getTime())
+          ? "ALL DAY"
+          : slot.toTimeString().slice(0, 5)}
+      </button>
     </div>
+  );
 }
 
-function CreateVenue({ loadEvents, user }: { loadEvents: () => void; user: user | undefined }) {
-    const [name, setName] = useState("");
-    const [otherInformation, setOtherInformation] = useState("");
-    const [accessibilityInformation, setAccessibilityInformation] = useState("");
-    const [pictureFile, setPictureFile] = useState<File>();
-    const [address, setAddress] = useState("");
-    const [capacity, setCapacity] = useState(0);
-    const [slotsAvailable, setSlotsAvailable] = useState<number[]>([]);
-    const navigate = useNavigate();
+const { VITE_SUBMISSIONS_OPEN } = import.meta.env;
 
-    async function uploadImage() {
-        if (pictureFile && user) {
-            const imageBuffer = await pictureFile.arrayBuffer();
-            const imageResponse = await postImage(pictureFile.name, imageBuffer, user.access_token);
-            const imageResult = await imageResponse.json();
-            return imageResult.content_uri;
-        }
-        return "";
+function CreateVenue({
+  loadEvents,
+  user,
+}: {
+  loadEvents: () => void;
+  user: user | undefined;
+}) {
+  const [name, setName] = useState("");
+  const [otherInformation, setOtherInformation] = useState("");
+  const [accessibilityInformation, setAccessibilityInformation] = useState("");
+  const [pictureFile, setPictureFile] = useState<File>();
+  const [address, setAddress] = useState("");
+  const [capacity, setCapacity] = useState(0);
+  const [slotsAvailable, setSlotsAvailable] = useState<number[]>([]);
+  const navigate = useNavigate();
+
+  if (VITE_SUBMISSIONS_OPEN !== "true") {
+    navigate("/venues");
+  }
+
+  async function uploadImage() {
+    if (pictureFile && user) {
+      const imageBuffer = await pictureFile.arrayBuffer();
+      const imageResponse = await postImage(
+        pictureFile.name,
+        imageBuffer,
+        user.access_token
+      );
+      const imageResult = await imageResponse.json();
+      return imageResult.content_uri;
     }
+    return "";
+  }
 
-    async function create() {
-        if (user) {
-            const picture = await uploadImage();
+  async function create() {
+    if (user) {
+      const picture = await uploadImage();
 
-            const response = await postVenue(
-                {
-                    name,
-                    otherInformation,
-                    accessibilityInformation,
-                    picture,
-                    address,
-                    creator: user.name,
-                    capacity,
-                    slotsAvailable,
-                },
-                user.access_token
-            );
-            if (response) {
-                navigate(`/venue/${response.event_id}`);
-                setName("");
-                setOtherInformation("");
-                setAccessibilityInformation("");
-                setAddress("");
-                setCapacity(0);
-                setSlotsAvailable([]);
-                loadEvents();
-            }
-        }
+      const response = await postVenue(
+        {
+          name,
+          otherInformation,
+          accessibilityInformation,
+          picture,
+          address,
+          creator: user.name,
+          capacity,
+          slotsAvailable,
+        },
+        user.access_token
+      );
+      if (response) {
+        navigate(`/venue/${response.event_id}`);
+        setName("");
+        setOtherInformation("");
+        setAccessibilityInformation("");
+        setAddress("");
+        setCapacity(0);
+        setSlotsAvailable([]);
+        loadEvents();
+      }
     }
+  }
 
-    function toggleSlot(slot: Date) {
-        const slotIndex = slotsAvailable.findIndex((slotAvailable) => slotAvailable === slot.getTime());
-        if (slotIndex >= 0) {
-            const newSlotsAvailable = slotsAvailable.slice(0, slotIndex).concat(slotsAvailable.slice(slotIndex + 1));
-            setSlotsAvailable(newSlotsAvailable);
-        } else {
-            const newSlotsAvailable = slotsAvailable.concat(slot.getTime());
-            setSlotsAvailable(newSlotsAvailable);
-        }
-    }
-
-    const days: ReactElement[][] = [
-        [<p className="day-heading">Monday 13th</p>, <SlotButton slotOn={slotsAvailable.includes(ALL_DAY_SLOTS.MONDAY)} slot={new Date(ALL_DAY_SLOTS.MONDAY)} toggleSlot={toggleSlot} />],
-        [<p className="day-heading">Tuesday 14th</p>, <SlotButton slotOn={slotsAvailable.includes(ALL_DAY_SLOTS.TUESDAY)} slot={new Date(ALL_DAY_SLOTS.TUESDAY)} toggleSlot={toggleSlot} />],
-        [<p className="day-heading">Wednesday 15th</p>, <SlotButton slotOn={slotsAvailable.includes(ALL_DAY_SLOTS.WEDNESDAY)} slot={new Date(ALL_DAY_SLOTS.WEDNESDAY)} toggleSlot={toggleSlot} />],
-        [<p className="day-heading">Thursday 16th</p>, <SlotButton slotOn={slotsAvailable.includes(ALL_DAY_SLOTS.THURSDAY)} slot={new Date(ALL_DAY_SLOTS.THURSDAY)} toggleSlot={toggleSlot} />],
-        [<p className="day-heading">Friday 17th</p>, <SlotButton slotOn={slotsAvailable.includes(ALL_DAY_SLOTS.FRIDAY)} slot={new Date(ALL_DAY_SLOTS.FRIDAY)} toggleSlot={toggleSlot} />],
-        [<p className="day-heading">Saturday 18th</p>, <SlotButton slotOn={slotsAvailable.includes(ALL_DAY_SLOTS.SATURDAY)} slot={new Date(ALL_DAY_SLOTS.SATURDAY)} toggleSlot={toggleSlot} />],
-        [<p className="day-heading">Sunday 19th</p>, <SlotButton slotOn={slotsAvailable.includes(ALL_DAY_SLOTS.SUNDAY)} slot={new Date(ALL_DAY_SLOTS.SUNDAY)} toggleSlot={toggleSlot} />],
-    ];
-
-    days.forEach((day, dayIndex) => {
-        for (let halfHourIndex = 0; halfHourIndex < 48; halfHourIndex++) {
-            const slot = new Date(MIDNIGHT13THUTC + halfHourIndex * 1000 * 60 * 30 + dayIndex * 1000 * 60 * 60 * 24);
-            const slotOn = slotsAvailable.includes(slot.getTime());
-
-            day.push(
-                <SlotButton slotOn={slotOn} slot={slot} toggleSlot={toggleSlot} />
-            );
-        }
-    });
-
-    const pictureUrl = pictureFile ? URL.createObjectURL(pictureFile) : "";
-
-    return (
-        <div className="creation-container">
-            <h1>New Venue</h1>
-            <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="name"></input>
-            <input
-                type="text"
-                value={otherInformation}
-                onChange={(e) => setOtherInformation(e.target.value)}
-                placeholder="other information"
-            ></input>
-            <input
-                type="text"
-                value={accessibilityInformation}
-                onChange={(e) => setAccessibilityInformation(e.target.value)}
-                placeholder="accessibility information"
-            ></input>
-            <p>Picture:</p>
-            {pictureFile && (
-                <>
-                    <img src={pictureUrl} className="create-image" />
-                    <br />
-                </>
-            )}
-            <input
-                type="file"
-                onChange={(e) => setPictureFile(e.target.files ? e.target.files[0] : undefined)}
-                accept="image/png, image/jpeg, image/gif"
-            />
-            <br />
-            <input
-                type="text"
-                value={address}
-                onChange={(e) => setAddress(e.target.value)}
-                placeholder="address (include postcode)"
-            ></input>
-            <br />
-            <label htmlFor="capacity">Capacity: </label>
-            <input
-                id="capacity"
-                type="number"
-                value={capacity}
-                onChange={(e) => setCapacity(parseInt(e.target.value))}
-            ></input>
-            <p>Time slots available [choose half hour slots within the week 13th-19th October]</p>
-            <div className="days-container">
-                {days.map((day) => (
-                    <div>{day}</div>
-                ))}
-            </div>
-            <button onClick={create}>Create</button>
-        </div>
+  function toggleSlot(slot: Date) {
+    const slotIndex = slotsAvailable.findIndex(
+      (slotAvailable) => slotAvailable === slot.getTime()
     );
+    if (slotIndex >= 0) {
+      const newSlotsAvailable = slotsAvailable
+        .slice(0, slotIndex)
+        .concat(slotsAvailable.slice(slotIndex + 1));
+      setSlotsAvailable(newSlotsAvailable);
+    } else {
+      const newSlotsAvailable = slotsAvailable.concat(slot.getTime());
+      setSlotsAvailable(newSlotsAvailable);
+    }
+  }
+
+  const days: ReactElement[][] = [
+    [
+      <p className="day-heading">Monday 13th</p>,
+      <SlotButton
+        slotOn={slotsAvailable.includes(ALL_DAY_SLOTS.MONDAY)}
+        slot={new Date(ALL_DAY_SLOTS.MONDAY)}
+        toggleSlot={toggleSlot}
+      />,
+    ],
+    [
+      <p className="day-heading">Tuesday 14th</p>,
+      <SlotButton
+        slotOn={slotsAvailable.includes(ALL_DAY_SLOTS.TUESDAY)}
+        slot={new Date(ALL_DAY_SLOTS.TUESDAY)}
+        toggleSlot={toggleSlot}
+      />,
+    ],
+    [
+      <p className="day-heading">Wednesday 15th</p>,
+      <SlotButton
+        slotOn={slotsAvailable.includes(ALL_DAY_SLOTS.WEDNESDAY)}
+        slot={new Date(ALL_DAY_SLOTS.WEDNESDAY)}
+        toggleSlot={toggleSlot}
+      />,
+    ],
+    [
+      <p className="day-heading">Thursday 16th</p>,
+      <SlotButton
+        slotOn={slotsAvailable.includes(ALL_DAY_SLOTS.THURSDAY)}
+        slot={new Date(ALL_DAY_SLOTS.THURSDAY)}
+        toggleSlot={toggleSlot}
+      />,
+    ],
+    [
+      <p className="day-heading">Friday 17th</p>,
+      <SlotButton
+        slotOn={slotsAvailable.includes(ALL_DAY_SLOTS.FRIDAY)}
+        slot={new Date(ALL_DAY_SLOTS.FRIDAY)}
+        toggleSlot={toggleSlot}
+      />,
+    ],
+    [
+      <p className="day-heading">Saturday 18th</p>,
+      <SlotButton
+        slotOn={slotsAvailable.includes(ALL_DAY_SLOTS.SATURDAY)}
+        slot={new Date(ALL_DAY_SLOTS.SATURDAY)}
+        toggleSlot={toggleSlot}
+      />,
+    ],
+    [
+      <p className="day-heading">Sunday 19th</p>,
+      <SlotButton
+        slotOn={slotsAvailable.includes(ALL_DAY_SLOTS.SUNDAY)}
+        slot={new Date(ALL_DAY_SLOTS.SUNDAY)}
+        toggleSlot={toggleSlot}
+      />,
+    ],
+  ];
+
+  days.forEach((day, dayIndex) => {
+    for (let halfHourIndex = 0; halfHourIndex < 48; halfHourIndex++) {
+      const slot = new Date(
+        MIDNIGHT13THUTC +
+          halfHourIndex * 1000 * 60 * 30 +
+          dayIndex * 1000 * 60 * 60 * 24
+      );
+      const slotOn = slotsAvailable.includes(slot.getTime());
+
+      day.push(
+        <SlotButton slotOn={slotOn} slot={slot} toggleSlot={toggleSlot} />
+      );
+    }
+  });
+
+  const pictureUrl = pictureFile ? URL.createObjectURL(pictureFile) : "";
+
+  return (
+    <div className="creation-container">
+      <h1>New Venue</h1>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="name"
+      ></input>
+      <input
+        type="text"
+        value={otherInformation}
+        onChange={(e) => setOtherInformation(e.target.value)}
+        placeholder="other information"
+      ></input>
+      <input
+        type="text"
+        value={accessibilityInformation}
+        onChange={(e) => setAccessibilityInformation(e.target.value)}
+        placeholder="accessibility information"
+      ></input>
+      <p>Picture:</p>
+      {pictureFile && (
+        <>
+          <img src={pictureUrl} className="create-image" />
+          <br />
+        </>
+      )}
+      <input
+        type="file"
+        onChange={(e) =>
+          setPictureFile(e.target.files ? e.target.files[0] : undefined)
+        }
+        accept="image/png, image/jpeg, image/gif"
+      />
+      <br />
+      <input
+        type="text"
+        value={address}
+        onChange={(e) => setAddress(e.target.value)}
+        placeholder="address (include postcode)"
+      ></input>
+      <br />
+      <label htmlFor="capacity">Capacity: </label>
+      <input
+        id="capacity"
+        type="number"
+        value={capacity}
+        onChange={(e) => setCapacity(parseInt(e.target.value))}
+      ></input>
+      <p>
+        Time slots available [choose half hour slots within the week 13th-19th
+        October]
+      </p>
+      <div className="days-container">
+        {days.map((day) => (
+          <div>{day}</div>
+        ))}
+      </div>
+      <button onClick={create}>Create</button>
+    </div>
+  );
 }
 
 export default CreateVenue;
--- src/Home.tsx
+++ src/Home.tsx
@@ -6,115 +6,143 @@ import { getImage } from "./requests";
 import { breakIntoDays, formatTime } from "./Event";
 
 function Event({ event, venues }: { event: event; venues: venue[] }) {
-    const [imageSrc, setImageSrc] = useState("");
+  const [imageSrc, setImageSrc] = useState("");
 
-    async function loadImage() {
-        if (event.picture) {
-            const pictureResponse = await getImage(event.picture.replace("mxc://", ""));
-            const pictureResult = await pictureResponse.blob();
-            const imageUrl = window.URL.createObjectURL(pictureResult);
-            setImageSrc(imageUrl);
-        }
+  async function loadImage() {
+    if (event.picture) {
+      const pictureResponse = await getImage(
+        event.picture.replace("mxc://", "")
+      );
+      const pictureResult = await pictureResponse.blob();
+      const imageUrl = window.URL.createObjectURL(pictureResult);
+      setImageSrc(imageUrl);
     }
+  }
 
-    useEffect(() => {
-        loadImage();
-    }, []);
+  useEffect(() => {
+    loadImage();
+  }, []);
 
-    const venue = venues.find((venue) => venue.id === event.venueId);
-    const time = breakIntoDays(event.slotsUsed).map(day => formatTime(day)).join(", ");
+  const venue = venues.find((venue) => venue.id === event.venueId);
+  const time = breakIntoDays(event.slotsUsed)
+    .map((day) => formatTime(day))
+    .join(", ");
 
-    return (
-        <div className="event">
-            <img src={imageSrc || "/reader.svg"} className="event-image" />
-            <div className="event-right">
-                <Link to={`/event/${event.id}`}>
-                    <h2>{event.name}</h2>
-                </Link>
-                {venue && (
-                    <p>
-                        Venue: <Link to={`/venue/${venue.id}`}>{venue.name}</Link>
-                    </p>
-                )}
-                <p>Time: {time}</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="event">
+      <img src={imageSrc || "/reader.svg"} className="event-image" />
+      <div className="event-right">
+        <Link to={`/event/${event.id}`}>
+          <h2>{event.name}</h2>
+        </Link>
+        {venue && (
+          <p>
+            Venue: <Link to={`/venue/${venue.id}`}>{venue.name}</Link>
+          </p>
+        )}
+        <p>Time: {time}</p>
+      </div>
+    </div>
+  );
 }
 
-function Home({ venues, events, user }: { venues: venue[]; events: event[]; user: user | undefined }) {
-    const { VITE_SUBMISSIONS_OPEN } = import.meta.env;
+function Home({
+  venues,
+  events,
+  user,
+}: {
+  venues: venue[];
+  events: event[];
+  user: user | undefined;
+}) {
+  const { VITE_SUBMISSIONS_OPEN } = import.meta.env;
 
-    const [dayActive, setDayActive] = useState(-1);
+  const [dayActive, setDayActive] = useState(-1);
 
-    const days: day[] = [
-        { name: "Monday 13th October", date: new Date(1760310000000), events: [] },
-        { name: "Tuesday 14th October", date: new Date(1760396400000), events: [] },
-        { name: "Wednesday 15th October", date: new Date(1760482800000), events: [] },
-        { name: "Thursday 16th October", date: new Date(1760569200000), events: [] },
-        { name: "Friday 17th October", date: new Date(1760655600000), events: [] },
-        { name: "Saturday 18th October", date: new Date(1760742000000), events: [] },
-        { name: "Sunday 19th October", date: new Date(1760828400000), events: [] },
-    ];
+  const days: day[] = [
+    { name: "Monday 13th October", date: new Date(1760310000000), events: [] },
+    { name: "Tuesday 14th October", date: new Date(1760396400000), events: [] },
+    {
+      name: "Wednesday 15th October",
+      date: new Date(1760482800000),
+      events: [],
+    },
+    {
+      name: "Thursday 16th October",
+      date: new Date(1760569200000),
+      events: [],
+    },
+    { name: "Friday 17th October", date: new Date(1760655600000), events: [] },
+    {
+      name: "Saturday 18th October",
+      date: new Date(1760742000000),
+      events: [],
+    },
+    { name: "Sunday 19th October", date: new Date(1760828400000), events: [] },
+  ];
 
-    events.forEach((event) => {
-        const slots = event.slotsUsed;
+  events.forEach((event) => {
+    const slots = event.slotsUsed;
 
-        if (slots) {
-            const eventDays = days.filter((day) => {
-                const slotsOnThisDay = slots.filter(slot => {
-                    const slotDate = new Date(slot);
-                    return day.date.getDate() === slotDate.getDate();
-                });
-                return slotsOnThisDay.length > 0;
-            });
-            eventDays.forEach(eventDay => eventDay.events.push(event));
-        }
-    });
+    if (slots) {
+      const eventDays = days.filter((day) => {
+        const slotsOnThisDay = slots.filter((slot) => {
+          const slotDate = new Date(slot);
+          return day.date.getDate() === slotDate.getDate();
+        });
+        return slotsOnThisDay.length > 0;
+      });
+      eventDays.forEach((eventDay) => eventDay.events.push(event));
+    }
+  });
 
-    return VITE_SUBMISSIONS_OPEN === "true" ? (
-        <>
-            <div className="backed-text-container">
-                <p className="indent">Event submissions open: 20th July - 31st August</p>
-                <p className="indent">
-                    View the schedule of events below, and go to <Link to="/instructions">Instructions</Link> to find
-                    out how to register your own event!
-                </p>
-            </div>
-            <div className="day-container">
-                {days.map((day, dayIndex) =>
-                    dayIndex === dayActive ? (
-                        <>
-                            <button className="day-button" onClick={() => setDayActive(-1)}>
-                                <h2>{day.name}</h2>
-                            </button>
-                            {day.events.map((event) => (
-                                <Event event={event} venues={venues} key={event.id} />
-                            ))}
-                            {day.events.length === 0 && <p className="backed-text">No events on this day</p>}
-                        </>
-                    ) : (
-                        <>
-                            <button className="day-button" onClick={() => setDayActive(dayIndex)}>
-                                <h2>{day.name}</h2>
-                            </button>
-                        </>
-                    )
-                )}
-                {user && (
-                    <Link to="/event/new">
-                        <h2 className="create-link">Create event</h2>
-                    </Link>
-                )}
-            </div>
-        </>
-    ) : (
-        <div id="coming-soon-container">
-            <h1>Antiuniversity is down for maintenance.</h1>
-            <p>The festival is on the 13th - 19th October</p>
-            <p>Sorry for the inconvenience, we're working hard to bring the site back</p>
+  return (
+    <>
+      {VITE_SUBMISSIONS_OPEN === "true" && (
+        <div className="backed-text-container">
+          <p className="indent">
+            Event submissions open: 20th July - 31st August
+          </p>
+          <p className="indent">
+            View the schedule of events below, and go to{" "}
+            <Link to="/instructions">Instructions</Link> to find out how to
+            register your own event!
+          </p>
         </div>
-    );
+      )}
+      <div className="day-container">
+        {days.map((day, dayIndex) =>
+          dayIndex === dayActive ? (
+            <>
+              <button className="day-button" onClick={() => setDayActive(-1)}>
+                <h2>{day.name}</h2>
+              </button>
+              {day.events.map((event) => (
+                <Event event={event} venues={venues} key={event.id} />
+              ))}
+              {day.events.length === 0 && (
+                <p className="backed-text">No events on this day</p>
+              )}
+            </>
+          ) : (
+            <>
+              <button
+                className="day-button"
+                onClick={() => setDayActive(dayIndex)}
+              >
+                <h2>{day.name}</h2>
+              </button>
+            </>
+          )
+        )}
+        {VITE_SUBMISSIONS_OPEN === "true" && user && (
+          <Link to="/event/new">
+            <h2 className="create-link">Create event</h2>
+          </Link>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default Home;
